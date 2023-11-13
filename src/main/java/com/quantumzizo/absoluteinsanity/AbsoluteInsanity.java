8fa85f37d4741ff5bff992ec778b37d2db296cb9
QuantumZizo <zayn.elborno@13bananas.com>
2023-11-13 13:58:32 -0700

    Customized Mod's files to my own.

---
+++ src/main/java/com/quantumzizo/absoluteinsanity/AbsoluteInsanity.java
@@ -0,0 +1,30 @@
+package com.quantumzizo.absoluteinsanity;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(AbsoluteInsanity.MOD_ID)
+public class AbsoluteInsanity {
+	public static final String MOD_ID = "absoluteinsanity";
+	
+	private static final Logger LOGGER = LogUtils.getLogger();
+	
+	public AbsoluteInsanity() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		
+		MinecraftForge.EVENT_BUS.register(this);
+		
+		modEventBus.addListener(this::commonSetup);
+	}
+	
+	private void commonSetup(final FMLCommonSetupEvent event) {
+		LOGGER.info("Don't ya just love going insane?");
+	}
+}
